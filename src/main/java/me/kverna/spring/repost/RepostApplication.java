6862573b0f376b8621afb40278db7967199f48e1
Espen <espenk95@gmail.com>
2020-03-07 13:36:04 +0100

    Initial commit

---
+++ src/main/java/me/kverna/spring/repost/RepostApplication.java
@@ -0,0 +1,13 @@
+package me.kverna.spring.repost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepostApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/kverna/spring/repost/RepostApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kverna.spring.repost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
