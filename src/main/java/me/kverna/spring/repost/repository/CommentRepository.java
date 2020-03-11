8df25c9b2edc29e713ff3be6ec8e4d0918830764
Espen <espenk95@gmail.com>
2020-03-11 19:09:06 +0100

    Add EditPost and extra methods in repositories.

--- src/main/java/me/kverna/spring/repost/repository/CommentRepository.java
+++ src/main/java/me/kverna/spring/repost/repository/CommentRepository.java
@@ -4,6 +4,13 @@ import me.kverna.spring.repost.data.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
+    List<Comment> findAllByAuthorUsername(String username);
+
+    List<Comment> findAllByParentResubName(String name);
+
+    List<Comment> findAllByParentPostId(int postId);
 }
--- src/main/java/me/kverna/spring/repost/repository/UserRepository.java
+++ src/main/java/me/kverna/spring/repost/repository/UserRepository.java
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<User, Integer> {
     User findByUsername(String username);
 
-    Void deleteByUsername(String username);
+    void deleteByUsername(String username);
 }
