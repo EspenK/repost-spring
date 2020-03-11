3e34a843fbf600a6fb00f67724dbc70a4d07e844
Espen <espenk95@gmail.com>
2020-03-11 16:17:19 +0100

    Move @Bean PasswordEncoder from application class to SecurityConfig

--- src/main/java/me/kverna/spring/repost/RepostApplication.java
+++ src/main/java/me/kverna/spring/repost/RepostApplication.java
@@ -2,19 +2,11 @@ package me.kverna.spring.repost;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class RepostApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RepostApplication.class, args);
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
