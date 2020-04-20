95e42bdedb23c80e1e8f1b93a09ca48f5d128cf5
Per Christian Kvalvik <pc@kvalvik.no>
2020-04-20 13:57:43 +0200

    Change authorize to check OAuth2 scope.
    This gives the API a stronger cohesion with OAuth2. Currently, Spring's default behaviour is to select all scopes unless specified. Since there is only one scope, it will always be selected.

--- src/main/java/me/kverna/spring/repost/security/AuthorizeUser.java
+++ src/main/java/me/kverna/spring/repost/security/AuthorizeUser.java
@@ -1,14 +1,13 @@
 package me.kverna.spring.repost.security;
 
-import org.springframework.security.access.prepost.PreAuthorize;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.TYPE})
-@PreAuthorize("hasAuthority('USER')")
+@PreAuthorize("#oauth2.hasScope('user')")
 public @interface AuthorizeUser {
 }
